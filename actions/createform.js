e3120e15d0a50d2f6fac7efcbda0145f58bf8de3
rowik <rowik@onet.eu>
2014-04-01 11:50:47 +0200

    Update createform.js

--- actions/createform.js
+++ actions/createform.js
@@ -5,29 +5,31 @@ var S3Form = require("../s3post").S3Form;
 var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
-var KEY_FIELD_NAME="AWSAcessKeyId";
+
+
 
 var task = function(request, callback){
 	//1. load configuration
-	var awsConfig=helpers.readJSONFile(AWS_CONFIG_FILE);
-	var policyData= helpers.readJSONFile(POLICY_FILE);
+	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
+	var policyData = helpers.readJSONFile(POLICY_FILE);
 
 	//2. prepare policy
-	var policy =new Policy(policyData);
+	var policy = new Policy(policyData);
 
 	//3. generate form fields for S3 POST
-	var s3Form= new S3Form(policy);
-	//4. get bucket name
-	var fields=s3form.generateS3FormFields();
-	
-	
-	fields.push(s3Form.addHiddenField(KEY_FIELD_NAME, awsConfig.accessKeyId));
-	
-	bucket=policy.getConditionValueByKey("bucket");
-	
-	
+	var s3Form = new S3Form(policy);
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:[], bucket:""}});
+	var fields = s3Form.generateS3FormFields();
+
+	fields.concat(s3Form.generateS3CredientalsFields(awsConfig));
+
+
+	//4. get bucket name
+	var bucket = policy.getConditionValueByKey("bucket");	
+
+	callback(null, {template: INDEX_TEMPLATE, 
+		params:{fields:fields, bucket:bucket}
+	});
 }
 
 exports.action = task;
