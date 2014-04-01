d1c1c2e5c1399b0d426f0df32cad54d197d0489a
rowik <rowik@onet.eu>
2014-04-01 11:50:06 +0200

    Update s3digest.js

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,11 +1,16 @@
 var AWS = require("aws-sdk");
 
+var BUCKET_PARAM_NAME = "bucket";
+var KEY_PARAM_NAME = "key";
+
+
+
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
 
-	
-	
+	callback(null, request.query);
+
 }
 
 exports.action = task
